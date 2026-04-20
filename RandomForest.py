4b3542085234ba58c6553d4e4c787acd3459c0cc
Shivpratap Rathore <shivrathore6999@gmail.com>
2026-04-20 17:41:21 +0530

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -1,1 +1,19 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier(n_estimators=100)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
